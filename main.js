0b64a2986ab0db52ac4759ac5594072d7edb1638
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-03 19:49:02 -0300

    Criação de uma referência para a lista de teclas

--- main.js
+++ main.js
@@ -2,9 +2,4 @@ function tocaSomPom() {
     document.querySelector("#som_tecla_pom").play();
 }
 
-function tocaSomClap() {
-    document.querySelector("#som_tecla_clap").play();
-}
-
-document.querySelector(".tecla_pom").onclick = tocaSomPom;
-document.querySelector(".tecla_clap").onclick = tocaSomClap;
+const listaDeTeclas = document.querySelectorAll(".tecla");
