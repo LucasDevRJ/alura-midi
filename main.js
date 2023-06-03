e2898869d283c1ac938eed8988b24ebbefdd20b6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-03 19:16:15 -0300

    Função que executa o som da tecla clap

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomPom() {
     document.querySelector(".tecla_pom");
     document.querySelector("#som_tecla_pom").play();
 }
+
+function tocaSomClap() {
+    document.querySelector(".tecla_clap");
+    document.querySelector("#som_tecla_clap").play();
+}
