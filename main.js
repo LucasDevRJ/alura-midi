0097ba620fcb0db82c53c253f90a464b90b482de
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-04 18:53:19 -0300

    Função onclick dentro de uma estrutura de repetição

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ function tocaSomPom() {
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
-listaDeTeclas[0].onclick = tocaSomPom;
+//listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador++;
+}
