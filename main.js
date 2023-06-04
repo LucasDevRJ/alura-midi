f6eaff314383fb104d87846f04ad13eeec47ea7b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-04 19:09:25 -0300

    Passagem de parâmetro na função para tocar som

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom() {
-    document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -8,6 +8,6 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom();
     contador++;
 }
