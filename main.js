377c94c43d6eefd0c5c24bb9ac73dd72b7319beb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-04 19:41:00 -0300

    Lógica para emitir o som de cada tecla concluida

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
-
 const listaDeTeclas = document.querySelectorAll(".tecla");
-//listaDeTeclas[0].onclick = tocaSomPom;
 
 let contador = 0;
-
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function() {
-        tocaSom("#som_tecla_pom");
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function() {
+        tocaSom(idAudio);
     }
     contador++;
 }
