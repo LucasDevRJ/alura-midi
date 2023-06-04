33f87efed732cf49c66b942c452377fdfb5875d1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-04 19:52:00 -0300

    Utilizando o for ao invés do while

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
+
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-while (contador < listaDeTeclas.length) {
-    const tecla = listaDeTeclas[contador];
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
     tecla.onclick = function() {
         tocaSom(idAudio);
     }
-    contador++;
 }
