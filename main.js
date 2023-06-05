c7837ba6981056843f4f2027014ae34f12dcc826
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-05 08:56:19 -0300

    Adição de classe do CSS no jS

--- main.js
+++ main.js
@@ -8,7 +8,16 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
+
     tecla.onclick = function() {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function() {
+        tecla.classList.add("ativa");
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove("ativa");
     }
 }
